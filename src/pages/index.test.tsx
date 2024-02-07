4951dfefcd630cc1d986167720f0ccf8714ad57a
sulthanftr <sulthanftr@gmail.com>
2024-02-07 16:49:19 +0700

    refactor: convert to Pages router

---
+++ src/pages/index.test.tsx
@@ -0,0 +1,23 @@
+import { render, screen } from '@testing-library/react';
+import Home, { getStaticProps } from './index';
+
+describe('Home', () => {
+  const mockData = 'Response from back end: Hello User1!';
+
+  beforeEach(() => {
+    const jsx = Home({ data: mockData });
+    render(jsx);
+  });
+
+  it(`should return 'Hello World!'`, () => {
+    expect(screen.getByRole('heading', { level: 1 })).toHaveTextContent(
+      'Hello World!',
+    );
+  });
+
+  it(`should show data fetched from the backend`, () => {
+    expect(screen.getByRole('heading', { level: 2 })).toHaveTextContent(
+      'Hello User1!',
+    );
+  });
+});
---
+++ src/pages/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+export default function Home({ data }: { data: string }) {
+  return (
+    <div>
+      <h1>Hello World!</h1>
+      <h2>{data}</h2>
+    </div>
+  );
+}
+
+export async function getStaticProps() {
+  try {
+    const res = await fetch(`${process.env.API_URL}/hello/user1`);
+    const data = 'Response from back end: ' + (await res.text());
+    return {
+      props: {
+        data,
+      },
+    };
+  } catch (error) {
+    console.error('Fetch error:', error);
+    return {
+      props: {
+        data: 'An error occurred while fetching data!',
+      },
+    };
+  }
+}
