a01d95e1a1697f549ee99d58ae078cb2115991b1
sulthanftr <sulthanftr@gmail.com>
2024-02-20 15:54:47 +0700

    use local font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,12 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
-import { Header } from '@/components/elements/Header';
+import { Header } from '@/components/elements';
+import theme from '@/styles/theme';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Header />
       <Component {...pageProps} />
     </ChakraProvider>
