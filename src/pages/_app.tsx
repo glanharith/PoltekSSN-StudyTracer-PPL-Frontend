0dcd3e8a31aba18d83accc0e1e8ae8adfde736c0
sulthanftr <sulthanftr@gmail.com>
2024-02-22 17:56:02 +0700

    add footer

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
-import { Header } from '@/components/elements';
+import { Header, Footer } from '@/components/elements';
 import theme from '@/styles/theme';
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -9,6 +9,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <ChakraProvider theme={theme}>
       <Header />
       <Component {...pageProps} />
+      <Footer />
     </ChakraProvider>
   );
 }
