2a31bb125db1f1f6554bd0bf29aa1ca855a1e1e1
sulthanftr <sulthanftr@gmail.com>
2024-02-24 23:10:16 +0700

    [GREEN] implement 'create study program' function

--- src/components/elements/StudyProgramModal/index.tsx
+++ src/components/elements/StudyProgramModal/index.tsx
@@ -1,6 +1,20 @@
-import { Modal, ModalContent, ModalOverlay } from '@chakra-ui/react';
+import {
+  Button,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  useToast,
+} from '@chakra-ui/react';
+import { axios } from '@/utils';
 import React from 'react';
+import { useForm } from 'react-hook-form';
 import { ModalProps, StudyProgramInput } from './interface';
+import { MdTitle } from 'react-icons/md';
+import { CustomAuthInput } from '@/components';
 
 export default function CreateStudyProgramModal({
   isOpen,
@@ -9,10 +23,84 @@ export default function CreateStudyProgramModal({
   studyProgramId,
   studyProgramName,
 }: ModalProps) {
+  const {
+    handleSubmit,
+    register,
+    setError,
+    formState: { errors, isSubmitting },
+  } = useForm<StudyProgramInput>({});
+  const toast = useToast();
+
+  const handleFormSubmit = async (data: StudyProgramInput) => {
+    try {
+      let successMessage: string;
+
+      await axios.post('/prodi', {
+        name: data.name,
+      });
+      successMessage = 'Berhasil membuat program studi!';
+
+      toast({
+        title: successMessage,
+        status: 'success',
+      });
+      onClose();
+    } catch (error: any) {
+      const status = error.response?.status;
+      let errorMessage;
+      if (status === 409) {
+        errorMessage = 'Nama program studi sudah digunakan!';
+      } else {
+        errorMessage = 'Gagal membuat program studi!';
+      }
+      setError('name', { message: errorMessage });
+    }
+  };
+
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
       <ModalOverlay />
-      <ModalContent borderRadius={48} p={8}></ModalContent>
+      <ModalContent borderRadius={48} p={8}>
+        <ModalHeader
+          textAlign={'center'}
+          textColor={'blue.900'}
+          fontSize={'3xl'}
+        >
+          Tambah Program Studi
+        </ModalHeader>
+        <ModalCloseButton p={6} borderRadius={32} />
+        <form onSubmit={handleSubmit(handleFormSubmit)}>
+          <ModalBody pb={6}>
+            <CustomAuthInput
+              name="name"
+              placeholder="Nama Program Studi"
+              error={errors.name?.message}
+              icon={MdTitle}
+              register={{
+                ...register('name', {
+                  required: 'Nama program studi tidak boleh kosong!',
+                }),
+              }}
+            />
+          </ModalBody>
+          <ModalFooter justifyContent={'center'}>
+            <Button onClick={onClose} mr={3}>
+              Batal
+            </Button>
+            <Button
+              bg="blue.500"
+              _hover={{
+                bg: 'blue.600',
+              }}
+              color="white"
+              isLoading={isSubmitting}
+              type="submit"
+            >
+              Buat
+            </Button>
+          </ModalFooter>
+        </form>
+      </ModalContent>
     </Modal>
   );
 }
