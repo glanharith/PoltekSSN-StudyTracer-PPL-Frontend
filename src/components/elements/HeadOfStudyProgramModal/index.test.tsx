b8a29cca552c1143be9008eedf765a1d17a5ae72
yudiptr <yudiputrasabri789@gmail.com>
2024-03-01 15:35:29 +0700

    [RED] create unit test for modal head of study program

---
+++ src/components/elements/HeadOfStudyProgramModal/index.test.tsx
@@ -0,0 +1,123 @@
+import React from 'react';
+import { render, screen, waitFor, fireEvent} from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { axios } from '@/utils';
+import MockAdapter from 'axios-mock-adapter';
+import HeadOfStudyProgramModal from '.';
+import userEvent from "@testing-library/user-event";
+
+describe('Create Head of Study Program', () => {
+  const mockOnClose = jest.fn();
+  const mockAxios = new MockAdapter(axios);
+
+  beforeEach(() => {
+    mockAxios.reset();
+    mockOnClose.mockClear();
+  });
+
+  it('renders correctly', async () => {
+    const mockData = {
+      message: "Successfully got all study programs",
+      data: [
+        { id: "48e941a9-3319-4f4c-8a2e-5d6a3287bf89", name: "Ilmu Sandi" },
+        { id: "68393bf0-0d80-43a7-889b-c46186a18777", name: "Ilmu Siber" }
+      ]
+    };
+    mockAxios.onGet('/prodi').reply(200, mockData);
+
+    await render(
+      <HeadOfStudyProgramModal
+        isOpen={true}
+        onClose={mockOnClose}
+        method="CREATE"
+        refetchData={() => {}}
+      />
+    );
+    expect(screen.getByText('Buat')).toBeInTheDocument();
+    expect(screen.getByText('Tambah Kepala Program Studi')).toBeInTheDocument();
+
+    // Optionally, you can wait for the mock axios call to resolve
+    await waitFor(() => {
+      expect(mockAxios.history.get.length).toBe(1);
+    });
+  });
+
+  it('validates empty name and shows error message', async () => {
+    const mockData = {
+      message: "Successfully got all study programs",
+      data: [
+        { id: "48e941a9-3319-4f4c-8a2e-5d6a3287bf89", name: "Ilmu Sandi" },
+        { id: "68393bf0-0d80-43a7-889b-c46186a18777", name: "Ilmu Siber" }
+      ]
+    };
+
+    mockAxios.onGet('/prodi').reply(200, mockData);
+    render(
+      <HeadOfStudyProgramModal
+        refetchData={() => {}}
+        isOpen={true}
+        onClose={mockOnClose}
+        method="CREATE"
+      />,
+    );
+    fireEvent.click(screen.getByText('Buat'));
+    await waitFor(() => {
+      expect(
+        screen.getByText('Nama kepala program studi tidak boleh kosong!'),
+      ).toBeInTheDocument();
+    });
+  });
+
+  it('create kaprodi', async () => {
+    const mockData = {
+      message: "Successfully got all study programs",
+      data: [
+        { id: "48e941a9-3319-4f4c-8a2e-5d6a3287bf89", name: "Ilmu Sandi" },
+        { id: "68393bf0-0d80-43a7-889b-c46186a18777", name: "Ilmu Siber" }
+      ]
+    };
+
+    const result = {
+      "message": "Successfully created a new study program"
+    }
+
+    mockAxios.onGet('/prodi').reply(200, mockData);
+    mockAxios.onPost('/kaprodi').replyOnce(200, result);
+
+    await render(
+      <HeadOfStudyProgramModal
+        refetchData={mockOnClose}
+        isOpen={true}
+        onClose={mockOnClose}
+        method="CREATE"
+      />,
+    );
+
+    await waitFor(() => {
+      expect(screen.getByText('Ilmu Sandi')).toBeInTheDocument();
+    });
+
+    // Select an option from the dropdown
+    fireEvent.change(screen.getByPlaceholderText('Nama Kepala Program Studi'), { target: { value: 'John Doe' } });
+    fireEvent.change(screen.getByPlaceholderText('Email'), { target: { value: 'john@example.com' } });
+    fireEvent.change(screen.getByPlaceholderText('Password'), { target: { value: 'password123' } });
+    fireEvent.change(screen.getAllByText('Jurusan')[1], {
+      target: { value: '48e941a9-3319-4f4c-8a2e-5d6a3287bf89' },
+    });
+
+    userEvent.click(screen.getByText('Buat'))
+    await waitFor(() => {
+      console.log(mockAxios)
+      console.log(screen.getByText("Berhasil membuat kepala program studi!"))
+      // expect(mockAxios.history.post.length).toBe(1);
+      // expect(mockAxios.history.post[0].data).toEqual({
+      //   studyProgramId: 'selectedJurusan',
+      //   email: 'john@example.com',
+      //   name: 'John Doe',
+      //   password: 'password123',
+      // });
+      // expect(mockOnClose).toHaveBeenCalled();
+    });
+
+  });
+});
