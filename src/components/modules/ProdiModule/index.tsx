d039189e72aa861de8f9c647a6112d850d0830c8
Ghayda Rafa Hernawan <ghayda.rafa@ui.ac.id>
2024-03-03 01:25:30 +0700

    [GREEN] implement ContentSection & ProdiModule, add program-studi pages

--- src/components/modules/ProdiModule/index.tsx
+++ src/components/modules/ProdiModule/index.tsx
@@ -1,21 +1,36 @@
-import { Flex } from "@chakra-ui/react";
-import { useState } from "react";
+import { Flex, useToast } from "@chakra-ui/react";
+import { useState, useEffect } from "react";
 import { ContentSection } from "./sections";
+import { axios } from '@/utils';
 import { StudyProgram } from "./interface";
 
 export const ProdiModule = () => {
+  const toast = useToast();
 
   const [studyPrograms, setStudyPrograms] = useState<StudyProgram[]>([]);
 
   const fetchStudyPrograms = async () => {
     try {
-      
+      const response = await axios.get('/prodi');
+      setStudyPrograms(response.data.data);
+      console.log(response);
     } catch (error) {
-
+      toast({
+        title: 'Gagal',
+        description: 'Gagal memuat daftar program studi',
+        status: 'error',
+        duration: 3000,
+        isClosable: true,
+      });
     }
   };
 
+  useEffect(() => {
+    fetchStudyPrograms();
+  }, []);
+
   const refetchData = () => {
+    fetchStudyPrograms();
   }
 
   return (
