ce5e37cd00c36227ee8d6717607ad5dca24f9296
yudiptr <yudiputrasabri789@gmail.com>
2024-03-04 09:32:15 +0700

    feat(authorization hoc): authorization for FE by hoc

---
+++ src/components/hoc/kaprodiHoc.tsx
@@ -0,0 +1,39 @@
+// components/hoc/withAuthorization.tsx
+
+import { useRouter } from 'next/router';
+import { useEffect, useState, ComponentType } from 'react';
+import { parseUser } from '@/utils';
+
+const kaprodiHoc = <P extends object>(WrappedComponent: ComponentType<P>) => {
+  const AuthComponent: React.FC<P> = (props) => {
+    const [loading, setLoading] = useState<boolean>(true);
+    const router = useRouter();
+
+    useEffect(() => {
+      async function checkAuthorization() {
+        try {
+          const userData = await parseUser();
+          setLoading(false);
+          if (!userData) {
+            router.replace('/login');
+          } else if (userData.role !== 'HEAD_STUDY_PROGRAM') {
+            router.replace('/');
+          }
+        } catch (error) {
+          console.error('Error checking authorization:', error);
+        }
+      }
+      checkAuthorization();
+    }, []);
+
+    if (loading) {
+      return <div>Loading...</div>;
+    }
+
+    return <WrappedComponent {...props } />;
+  };
+
+  return AuthComponent;
+};
+
+export default kaprodiHoc;
--- src/pages/kepala-program-studi/index.tsx
+++ src/pages/kepala-program-studi/index.tsx
@@ -1,7 +1,8 @@
 import { KaprodiModule } from "@/components";
+import AdminHoc from "@/components/hoc/adminHoc";
 
 const KepalaProgramStudi = () => {
   return <KaprodiModule />;
 };
 
-export default KepalaProgramStudi;
+export default AdminHoc(KepalaProgramStudi);
