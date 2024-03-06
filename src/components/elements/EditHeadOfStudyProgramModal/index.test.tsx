dcb28da26fe9348dea26aef137684a6dc7d1046f
Ahmad Hanif Adisetya <ahmad.hanif11@ui.ac.id>
2024-03-07 06:19:42 +0700

    [REFACTOR](EditHeadModalTest) refactor: make mock constant global for the test cases

--- src/components/elements/EditHeadOfStudyProgramModal/index.test.tsx
+++ src/components/elements/EditHeadOfStudyProgramModal/index.test.tsx
@@ -9,6 +9,34 @@ import userEvent from "@testing-library/user-event";
 describe('Edit Head of Study Program Modal', () => {
   const mockOnClose = jest.fn();
   const mockAxios = new MockAdapter(axios);
+  const mockRefetchData = jest.fn();
+
+  const mockData = {
+    message: "Successfully got all study programs",
+    data: [
+      { id: "48e941a9-3319-4f4c-8a2e-5d6a3287bf89", name: "Ilmu Sandi" },
+      { id: "68393bf0-0d80-43a7-889b-c46186a18777", name: "Ilmu Siber" }
+    ]
+  };
+  const mockStudyProgram = {
+    name: "Ilmu Padi",
+    id: '4685953a-dd4a-4b3e-83a6-9bb80fcdf455'
+  };
+  const mockStudyProgram2 = {
+    name: "Ilmu Menyala",
+    id: '4685953a-dd4a-4b3e-83a6-9bb80fcdf466'
+  };
+  const mockKaprodi = {
+    id: '8912b2ba-a09b-401b-ba6d-bf4974f36789',
+    studyProgramId: mockStudyProgram.id
+  };
+  const mockUser = {
+    name: "Sunaryo",
+    email: "sunaryo@gmail.com",
+    password: "KetoprakSunaryo",
+    id: mockKaprodi.id,
+    role: "HEAD_STUDY_PROGRAM"
+  }
 
   beforeEach(() => {
     mockAxios.reset();
@@ -16,28 +44,6 @@ describe('Edit Head of Study Program Modal', () => {
   });
 
   it('renders correctly', async () => {
-    const mockData = {
-      message: "Successfully got all study programs",
-      data: [
-        { id: "48e941a9-3319-4f4c-8a2e-5d6a3287bf89", name: "Ilmu Sandi" },
-        { id: "68393bf0-0d80-43a7-889b-c46186a18777", name: "Ilmu Siber" }
-      ]
-    };
-    const mockStudyProgram = {
-      name: "Ilmu Padi",
-      id: '4685953a-dd4a-4b3e-83a6-9bb80fcdf455'
-    };
-    const mockKaprodi = {
-      id: '8912b2ba-a09b-401b-ba6d-bf4974f36789',
-      studyProgramId: mockStudyProgram.id
-    };
-    const mockUser = {
-      name: "Sunaryo",
-      email: "sunaryo@gmail.com",
-      password: "KetoprakSunaryo",
-      id: mockKaprodi.id,
-      role: "HEAD_STUDY_PROGRAM"
-    }
     mockAxios.onGet('/prodi').reply(200, mockData);
 
     await render(
@@ -57,28 +63,6 @@ describe('Edit Head of Study Program Modal', () => {
   });
 
   it('validates empty name and shows error message', async () => {
-    const mockData = {
-      message: "Successfully got all study programs",
-      data: [
-        { id: "48e941a9-3319-4f4c-8a2e-5d6a3287bf89", name: "Ilmu Sandi" },
-        { id: "68393bf0-0d80-43a7-889b-c46186a18777", name: "Ilmu Siber" }
-      ]
-    };
-    const mockStudyProgram = {
-      name: "Ilmu Padi",
-      id: '4685953a-dd4a-4b3e-83a6-9bb80fcdf455'
-    };
-    const mockKaprodi = {
-      id: '8912b2ba-a09b-401b-ba6d-bf4974f36789',
-      studyProgramId: mockStudyProgram.id
-    };
-    const mockUser = {
-      name: "Sunaryo",
-      email: "sunaryo@gmail.com",
-      password: "KetoprakSunaryo",
-      id: mockKaprodi.id,
-      role: "HEAD_STUDY_PROGRAM"
-    }
     mockAxios.onGet('/prodi').reply(200, mockData);
 
     await render(
@@ -101,32 +85,6 @@ describe('Edit Head of Study Program Modal', () => {
   });
 
   it('submits the form and calls API', async () => {
-    const mockData = {
-      message: "Successfully got all study programs",
-      data: [
-        { id: "48e941a9-3319-4f4c-8a2e-5d6a3287bf89", name: "Ilmu Sandi" },
-        { id: "68393bf0-0d80-43a7-889b-c46186a18777", name: "Ilmu Siber" }
-      ]
-    };
-    const mockStudyProgram = {
-      name: "Ilmu Padi",
-      id: '4685953a-dd4a-4b3e-83a6-9bb80fcdf455'
-    };
-    const mockStudyProgram2 = {
-      name: "Ilmu Menyala",
-      id: '4685953a-dd4a-4b3e-83a6-9bb80fcdf466'
-    };
-    const mockKaprodi = {
-      id: '8912b2ba-a09b-401b-ba6d-bf4974f36789',
-      studyProgramId: mockStudyProgram.id
-    };
-    const mockUser = {
-      name: "Sunaryo",
-      email: "sunaryo@gmail.com",
-      password: "KetoprakSunaryo",
-      id: mockKaprodi.id,
-      role: "HEAD_STUDY_PROGRAM"
-    }
     mockAxios.onGet('/prodi').reply(200, mockData);
 
     await render(
@@ -150,32 +108,6 @@ describe('Edit Head of Study Program Modal', () => {
   });
 
   it('invalid inputs and not able to call API', async () => {
-    const mockData = {
-      message: "Successfully got all study programs",
-      data: [
-        { id: "48e941a9-3319-4f4c-8a2e-5d6a3287bf89", name: "Ilmu Sandi" },
-        { id: "68393bf0-0d80-43a7-889b-c46186a18777", name: "Ilmu Siber" }
-      ]
-    };
-    const mockStudyProgram = {
-      name: "Ilmu Padi",
-      id: '4685953a-dd4a-4b3e-83a6-9bb80fcdf455'
-    };
-    const mockStudyProgram2 = {
-      name: "Ilmu Menyala",
-      id: '4685953a-dd4a-4b3e-83a6-9bb80fcdf466'
-    };
-    const mockKaprodi = {
-      id: '8912b2ba-a09b-401b-ba6d-bf4974f36789',
-      studyProgramId: mockStudyProgram.id
-    };
-    const mockUser = {
-      name: "Sunaryo",
-      email: "sunaryo@gmail.com",
-      password: "KetoprakSunaryo",
-      id: mockKaprodi.id,
-      role: "HEAD_STUDY_PROGRAM"
-    }
     mockAxios.onGet('/prodi').reply(200, mockData);
 
     await render(
