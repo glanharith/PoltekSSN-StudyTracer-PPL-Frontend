af26b6eb420dc7e0625e672ee65d6b8ee5ba8ab3
yudiptr <yudiputrasabri789@gmail.com>
2024-03-26 18:51:33 +0700

    fix(fill survey): add route to survey by id

--- src/components/modules/FillSurveyModule/index.tsx
+++ src/components/modules/FillSurveyModule/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   Box,
   Button,
@@ -14,71 +14,108 @@ import {
   SliderThumb,
   SliderTrack,
   SliderMark,
+  Flex,
+  useToast,
 } from '@chakra-ui/react';
 import { Survey } from './interface';
+import { axios } from '@/utils';
 
 interface Props {
-  survey: Survey;
+  surveyId: string;
 }
 
-const SurveyForm: React.FC<Props> = ({ survey }) => {
+const SurveyForm: React.FC<Props> = ({ surveyId }) => {
+  const [survey, setSurvey] = useState<Survey | undefined>();
+  const toast = useToast();
+
+  useEffect(() => {
+    const fetchSurvey = async () => {
+      try {
+        const response = await axios.get('/survey/' + surveyId);
+        setSurvey(response.data);
+      } catch (error) {
+        toast({
+          title: 'Gagal',
+          description: 'Gagal memuat daftar kepala program studi',
+          status: 'error',
+          duration: 3000,
+          isClosable: true,
+        });
+      }
+    };
+
+    fetchSurvey();
+  }, [surveyId, toast]);
+
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     // Add your form submission logic here
   };
 
   return (
-    <Box p={4}>
-      <Heading mb={4}>{survey.title}</Heading>
-      <Box mb={4}>{survey.description}</Box>
-      <form onSubmit={handleSubmit}>
-        <Stack spacing={4}>
-          {survey.questions.map((question) => (
-            <FormControl key={question.id}>
-              <FormLabel htmlFor={question.id}>{question.question}</FormLabel>
-              {question.type === 'TEXT' && (
-                <Input type="text" id={question.id} name={question.id} />
-              )}
-              {question.type === 'CHECKBOX' && (
-                <Stack spacing={2}>
-                  {question.option?.map((option) => (
-                    <Checkbox key={option.id} name={option.id} value={option.label}>
-                      {option.label}
-                    </Checkbox>
-                  ))}
-                </Stack>
-              )}
-              {question.type === 'RADIO' && (
-                <Stack spacing={2}>
-                  {question.option?.map((option) => (
-                    <Radio key={option.id} name={question.id} value={option.label}>
-                      {option.label}
-                    </Radio>
-                  ))}
-                </Stack>
-              )}
-              {question.type === 'RANGE' && (
-                <Box>
-                  <Slider aria-label={question.question} id={question.id} defaultValue={question.rangeFrom} min={question.rangeFrom} max={question.rangeTo}>
-                    <SliderTrack>
-                      <SliderFilledTrack />
-                    </SliderTrack>
-                    <SliderThumb />
-                    {Array.from({ length: question.rangeTo as number - (question.rangeFrom as number) + 1 }, (_, index) => (
-                      <SliderMark key={index} value={question.rangeFrom as number + index}>
-                        {question.rangeFrom as number + index}
-                      </SliderMark>
+    <Box>
+      <Flex justify={'center'}>
+        <Box p={4} w={'50%'}>
+          {survey && (
+            <Flex flexDirection={'column'} gap={'16px'}>
+              <Box rounded={'md'} bgColor={'gray.100'} padding={'20px'}>
+                <Heading textColor={'black'} mb={4}> {survey.title}</Heading>
+                <Box textColor={'black'} mb={4}>{survey.description}</Box>
+              </Box>
+              <Box rounded={'md'} bgColor={'gray.100'} padding={'16px'}>
+                <form onSubmit={handleSubmit}>
+                  <Stack spacing={4}>
+                    {survey.questions.map((question) => (
+                      <FormControl key={question.id}>
+                        <FormLabel htmlFor={question.id}>{question.order+1}. {question.question}</FormLabel>
+                        {question.type === 'TEXT' && (
+                          <Input type="text" id={question.id} name={question.id} />
+                        )}
+                        {question.type === 'CHECKBOX' && (
+                          <Stack spacing={2}>
+                            {question.option?.map((option) => (
+                              <Checkbox key={option.id} name={option.id} value={option.label}>
+                                {option.label}
+                              </Checkbox>
+                            ))}
+                          </Stack>
+                        )}
+                        {question.type === 'RADIO' && (
+                          <Stack spacing={2}>
+                            {question.option?.map((option) => (
+                              <Radio key={option.id} name={question.id} value={option.label}>
+                                {option.label}
+                              </Radio>
+                            ))}
+                          </Stack>
+                        )}
+                        {question.type === 'RANGE' && (
+                          <Box>
+                            <Slider aria-label={question.question} id={question.id} defaultValue={question.rangeFrom} min={question.rangeFrom} max={question.rangeTo}>
+                              <SliderTrack>
+                                <SliderFilledTrack />
+                              </SliderTrack>
+                              <SliderThumb />
+                              {Array.from({ length: question.rangeTo as number - (question.rangeFrom as number) + 1 }, (_, index) => (
+                                <SliderMark key={index} value={question.rangeFrom as number + index}>
+                                  {question.rangeFrom as number + index}
+                                </SliderMark>
+                              ))}
+                            </Slider>
+                          </Box>
+                        )}
+                      </FormControl>
                     ))}
-                  </Slider>
-                </Box>
-              )}
-            </FormControl>
-          ))}
-          <Button type="submit" colorScheme="blue">
-            Submit
-          </Button>
-        </Stack>
-      </form>
+                    <Button type="submit" colorScheme="blue">
+                      Submit
+                    </Button>
+                  </Stack>
+                </form>
+              </Box>
+            </Flex>
+          )}
+        </Box>
+      </Flex>
     </Box>
   );
 };
--- src/pages/survey/[surveyId].tsx
+++ src/pages/survey/[surveyId].tsx
@@ -7,88 +7,10 @@ import AlumniHoc from '@/components/hoc/alumniHoc';
 
 
 const FillSurvey =  () => {
-
-    const survey : Survey = {
-        "id": "8abf188d-2e08-4010-8e17-92fa97a6e4f2",
-        "type": "CURRICULUM",
-        "title": "JUDUL SURVEY",
-        "description": "DESKRIPSI SURVEY",
-        "startTime": "2024-03-25T15:24:00.000Z",
-        "endTime": "2024-03-29T15:24:00.000Z",
-        "questions": [
-            {
-                "id": "5a2ae682-d420-451f-a5e2-8d6f2947c50c",
-                "type": "TEXT",
-                "question": "INI TEKS",
-                "rangeFrom": 1,
-                "rangeTo": 3,
-                "order": 0,
-                "formId": "8abf188d-2e08-4010-8e17-92fa97a6e4f2",
-                "option": []
-            },
-            {
-                "id": "7f324de3-045c-49b7-bc35-073ded862b2a",
-                "type": "CHECKBOX",
-                "question": "INI CHECKBOX",
-                "rangeFrom": 1,
-                "rangeTo": 3,
-                "order": 1,
-                "formId": "8abf188d-2e08-4010-8e17-92fa97a6e4f2",
-                "option": [
-                    {
-                        "id": "70c93d40-92c3-4c5c-99f2-116f0cfa4b32",
-                        "label": "1",
-                        "questionId": "7f324de3-045c-49b7-bc35-073ded862b2a",
-                        "order": 0
-                    },
-                    {
-                        "id": "83348005-53ce-4cfe-b1c8-b1fbcf83a382",
-                        "label": "2",
-                        "questionId": "7f324de3-045c-49b7-bc35-073ded862b2a",
-                        "order": 1
-                    }
-                ]
-            },
-            {
-                "id": "b48e4647-5811-40f8-b046-8499b3371e0e",
-                "type": "RADIO",
-                "question": "INI RADIO",
-                "rangeFrom": 1,
-                "rangeTo": 3,
-                "order": 2,
-                "formId": "8abf188d-2e08-4010-8e17-92fa97a6e4f2",
-                "option": [
-                    {
-                        "id": "78e8e1fb-ed45-4e6a-af0c-83d973300cbc",
-                        "label": "1",
-                        "questionId": "b48e4647-5811-40f8-b046-8499b3371e0e",
-                        "order": 0
-                    },
-                    {
-                        "id": "4d197746-0248-4b10-975b-41a8aadb84c6",
-                        "label": "2",
-                        "questionId": "b48e4647-5811-40f8-b046-8499b3371e0e",
-                        "order": 1
-                    }
-                ]
-            },
-            {
-                "id": "e16d8860-8f30-4805-9b40-3d21bb789917",
-                "type": "RANGE",
-                "question": "INI SKALA",
-                "rangeFrom": 1,
-                "rangeTo": 5,
-                "order": 3,
-                "formId": "8abf188d-2e08-4010-8e17-92fa97a6e4f2",
-                "option": []
-            }
-        ]
-    }
-
     const router = useRouter();
-    const { surveyType } = router.query;
+    const { surveyId } = router.query;
 
-    return <SurveyForm survey={survey} />;
+    return <SurveyForm surveyId={surveyId as string} />;
 };
 
 export default AlumniHoc(FillSurvey)
