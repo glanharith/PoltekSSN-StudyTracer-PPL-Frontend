a8029a50092608df2b11534f4b4ac3c34768c913
yudiptr <yudiputrasabri789@gmail.com>
2024-03-26 20:55:41 +0700

    feat(fill survey): done setup fe, minus send data to BE

--- src/components/modules/FillSurveyModule/index.tsx
+++ src/components/modules/FillSurveyModule/index.tsx
@@ -20,6 +20,8 @@ import {
 } from '@chakra-ui/react';
 import { Survey } from './interface';
 import { axios } from '@/utils';
+import { useForm } from 'react-hook-form';
+
 
 interface Props {
   surveyId: string;
@@ -29,6 +31,7 @@ const SurveyForm: React.FC<Props> = ({ surveyId }) => {
   const [survey, setSurvey] = useState<Survey | undefined>();
   const [selectedOptions, setSelectedOptions] = useState<{ [key: string]: string }>({});
   const toast = useToast();
+  const { register, handleSubmit, formState: { errors } } = useForm();
 
   useEffect(() => {
     const fetchSurvey = async () => {
@@ -53,10 +56,48 @@ const SurveyForm: React.FC<Props> = ({ surveyId }) => {
     setSelectedOptions({ ...selectedOptions, [questionId]: optionLabel });
   };
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmite = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    // Add your form submission logic here
-    console.log(selectedOptions);
+  
+    const formData: { [key: string]: string | string[] | number } = {};
+  
+    // Iterate through each question in the survey
+    survey?.questions.forEach((question) => {
+      const questionId = question.id;
+  
+      // Handle different question types
+      switch (question.type) {
+        case 'TEXT':
+          const textInputValue = (document.getElementById(questionId) as HTMLInputElement).value;
+          formData[questionId] = textInputValue;
+          break;
+
+        case 'CHECKBOX':
+          const checkboxInputs = document.querySelectorAll(`input[name="${question.id}"]:checked`);
+          const checkboxValues = Array.from(checkboxInputs).map((checkbox) => {
+            return (checkbox as HTMLInputElement).value;
+          });
+          formData[question.id] = checkboxValues;
+          break;
+  
+        case 'RADIO':
+          const radioSelectedValue = selectedOptions[questionId];
+          formData[questionId] = radioSelectedValue;
+          break;
+  
+        case 'RANGE':
+          const sliderElement = document.getElementById("slider-thumb-" + questionId) as HTMLInputElement;
+          if (sliderElement) {
+            const sliderSelectedValue = parseFloat(sliderElement.ariaValueNow as string);
+            formData[questionId] = sliderSelectedValue;
+          } 
+          break;
+  
+        default:
+          break;
+      }
+    });
+    console.log(formData);
   };
 
   return (
@@ -73,7 +114,7 @@ const SurveyForm: React.FC<Props> = ({ surveyId }) => {
                 <Text textColor={'gray.500'} fontStyle={'italic'}>
                   Silahkan isi pertanyaan-pertanyaan berikut ini dengan jawaban yang sesuai.
                 </Text>
-                <form onSubmit={handleSubmit}>
+                <form onSubmit={handleSubmite}>
                   <Stack spacing={4}>
                     {survey.questions.map((question) => (
                       <FormControl key={question.id}>
@@ -83,8 +124,9 @@ const SurveyForm: React.FC<Props> = ({ surveyId }) => {
                         )}
                         {question.type === 'CHECKBOX' && (
                           <Stack spacing={2}>
+                            <Text textColor={'gray.500'} fontStyle={'italic'} fontSize={'12'}>Pilih opsi berikut</Text>
                             {question.option?.map((option) => (
-                              <Checkbox key={option.id} name={option.id} value={option.label}>
+                              <Checkbox key={option.id} name={question.id} value={option.label}>
                                 {option.label}
                               </Checkbox>
                             ))}
@@ -92,6 +134,7 @@ const SurveyForm: React.FC<Props> = ({ surveyId }) => {
                         )}
                         {question.type === 'RADIO' && (
                           <Stack spacing={2}>
+                            <Text textColor={'gray.500'} fontStyle={'italic'} fontSize={'12'}>Pilih salah satu</Text>
                             {question.option?.map((option) => (
                               <Radio
                                 key={option.id}
@@ -106,20 +149,31 @@ const SurveyForm: React.FC<Props> = ({ surveyId }) => {
                           </Stack>
                         )}
                         {question.type === 'RANGE' && (
-                          <Box>
-                            <Slider aria-label={question.question} id={question.id} defaultValue={question.rangeFrom} min={question.rangeFrom} max={question.rangeTo}>
-                              <SliderTrack>
-                                <SliderFilledTrack />
-                              </SliderTrack>
-                              <SliderThumb />
-                              {Array.from({ length: question.rangeTo as number - (question.rangeFrom as number) + 1 }, (_, index) => (
-                                <SliderMark key={index} value={question.rangeFrom as number + index}>
-                                  {question.rangeFrom as number + index}
-                                </SliderMark>
-                              ))}
-                            </Slider>
-                          </Box>
-                        )}
+                        <Box>
+                          <Text textColor={'gray.500'} fontStyle={'italic'} fontSize={'12'}>
+                            Range berikut adalah dari sangat tidak setuju menuju sangat setuju
+                          </Text>
+                          <Slider
+                            aria-label={question.question}
+                            id={question.id} 
+                            defaultValue={question.rangeFrom}
+                            min={question.rangeFrom}
+                            max={question.rangeTo}
+                            mt={4}
+                          >
+                            <SliderTrack>
+                              <SliderFilledTrack />
+                            </SliderTrack>
+                            <SliderThumb />
+                            {Array.from({ length: question.rangeTo as number - (question.rangeFrom as number) + 1 }, (_, index) => (
+                              <SliderMark key={index} value={question.rangeFrom as number + index} mt={2}>
+                                {question.rangeFrom as number + index}
+                              </SliderMark>
+                            ))}
+                          </Slider>
+                        </Box>
+                      )}
+
                       </FormControl>
                     ))}
                     <Button type="submit" colorScheme="blue">
