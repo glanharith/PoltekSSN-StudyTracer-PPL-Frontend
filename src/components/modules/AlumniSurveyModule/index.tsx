db319b81796e1edc5316fd834aa5e0f40b4453a6
Ghayda Rafa Hernawan <ghayda.rafa@ui.ac.id>
2024-03-31 17:44:17 +0700

    [RED] add AlumniSurveyModule test

---
+++ src/components/modules/AlumniSurveyModule/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { AlumniSurveyModuleProps } from './interface';
+
+const AlumniSurveyModule: React.FC<AlumniSurveyModuleProps> = ({
+  surveyType,
+}) => {
+  const fetchProfile = async () => {};
+
+  const fetchSurvey = async () => {};
+
+  return <div></div>;
+};
+
+export default AlumniSurveyModule;
