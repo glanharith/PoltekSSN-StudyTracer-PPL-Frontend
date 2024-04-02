bfed6b69b56eddbff6e59929d25b882321eae020
Ghayda Rafa Hernawan <ghayda.rafa@ui.ac.id>
2024-04-02 21:29:33 +0700

    [GREEN] (SurveyAccordion) implement accordion w survey card

--- src/components/elements/SurveyAccordion/index.tsx
+++ src/components/elements/SurveyAccordion/index.tsx
@@ -3,19 +3,52 @@ import {
   AccordionButton,
   AccordionIcon,
   AccordionPanel,
+  Grid,
+  GridItem,
+  Box,
+  Text,
 } from '@chakra-ui/react';
 import { AccordionProps } from './interface';
+import SurveyCard from '../SurveyCard';
+
 export default function SurveyAccordion({
   title,
   surveys,
-  admin,
+  admin
 }: AccordionProps) {
   return (
     <AccordionItem>
-      <AccordionButton>
-        <AccordionIcon />
-      </AccordionButton>
-      <AccordionPanel></AccordionPanel>
+      <h2>
+        <AccordionButton _expanded={{ bg: 'blue.900', color: 'white' }}>
+          <Box as="span" flex="1" textAlign="left" fontWeight="bold">
+            {title}
+          </Box>
+          <AccordionIcon />
+        </AccordionButton>
+      </h2>
+      <AccordionPanel pb={2} background={'gray.100'}>
+        {surveys.length > 0 ? (
+          <Grid templateColumns="repeat(3, 1fr)" gap={2} margin={5}>
+            {surveys.map((survey) => (
+              <GridItem key={survey.id}>
+                <SurveyCard
+                  survey={survey}
+                  fillButton={false}
+                  deleteButton={admin}
+                  editButton={admin}
+                  downloadButton={true}
+                  previewButton={admin}
+                  isDisabled={false}
+                />
+              </GridItem>
+            ))}
+          </Grid>
+        ) : (
+          <Text textAlign="center" color="gray.600" m={4}>
+            Belum ada survey
+          </Text>
+        )}
+      </AccordionPanel>
     </AccordionItem>
   );
 }
