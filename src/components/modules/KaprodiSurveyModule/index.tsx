1a3567be15a66014d5fa04ea9b950712d3b679a7
Ghayda Rafa Hernawan <ghayda.rafa@ui.ac.id>
2024-04-03 01:31:39 +0700

    [GREEN] (KaprodiSurveyModule)  implement Kaprodi Module to display all surveys

--- src/components/modules/KaprodiSurveyModule/index.tsx
+++ src/components/modules/KaprodiSurveyModule/index.tsx
@@ -1,7 +1,31 @@
 import React from 'react';
+import {
+  Text,
+  Flex,
+} from '@chakra-ui/react';
+import { useRouter } from 'next/router';
+import AllSurveys from '@/components/elements/AllSurveys';
 
 const KaprodiSurveyModule = () => {
-  return <div style={{ minHeight: '100vh' }}></div>;
+  const router = useRouter();
+
+  return (
+    <div style={{ minHeight: '100vh' }}>
+      
+        <Flex justify="center" align="center" flex="1">
+          <Text
+            color={'blue.900'}
+            fontSize={{ base: 28, md: 30 }}
+            fontWeight="bold"
+            m={4}
+          >
+            Daftar Survey
+          </Text>
+        </Flex>
+
+      <AllSurveys role={'HEAD'}></AllSurveys>
+    </div>
+  );
 };
 
 export default KaprodiSurveyModule;
