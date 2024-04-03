2353975ccf5a06560d236facd07b5345194227ba
Ghayda Rafa Hernawan <ghayda.rafa@ui.ac.id>
2024-04-03 15:33:57 +0700

    add /karir and /kurikulum page

---
+++ src/pages/survey/karir.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import AlumniSurveyModule from '@/components/modules/AlumniSurveyModule';
+
+const AlumniSurveyKarir = () => {
+  return (
+    <AlumniSurveyModule surveyType="CAREER" />
+  );
+};
+
+export default AlumniSurveyKarir;
