05be4f8b22165264cc6ec24284f490c3202886dd
Ahmad Hanif Adisetya <ahmad.hanif11@ui.ac.id>
2024-05-07 13:17:58 +0700

    chore: add chart and resize observer dependencies

--- jest.setup.ts
+++ jest.setup.ts
@@ -2,5 +2,7 @@
 import '@testing-library/jest-dom';
 import 'whatwg-fetch';
 import { TextEncoder, TextDecoder } from 'util';
+import ResizeObserver from 'resize-observer-polyfill';
 
 Object.assign(global, { TextDecoder, TextEncoder });
+global.ResizeObserver = ResizeObserver;
